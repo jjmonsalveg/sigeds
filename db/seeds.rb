db3358f2bc4ae39e494d4780382185c9c4d26c61
Jonathan J. Monsalve <j.jmonsalveg@gmail.com>
2014-09-30 00:41:47 -0430

    nueva semilla

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,19 +5,17 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
-User.create!(name:  "Gerente DS Ejemplo",
-             email: "gerenteds@railstutorial.org",
+User.create!(email: "gerenteds@railstutorial.org",
              password:              "foobar",
              password_confirmation: "foobar",
              rol: "gerente_ds")
 
-2.times do |n|
-  name  = Faker::Name.name
+4.times do |n|
+  # name  = Faker::Name.name
   email = "example-#{n+1}@railstutorial.org"
   password = "password"
   rol = "basico"
-  User.create!(name:  name,
-               email: email,
+  User.create!(email: email,
                password:              password,
                password_confirmation: password,
                rol: rol)
